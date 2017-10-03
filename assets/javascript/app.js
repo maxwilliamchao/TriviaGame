71bff59d8f1434876d2aecc1407a69fece34b1e5
maxwilliamchao <maxwilliamchao@gmail.com>
2017-10-03 08:53:18 -0700

    notes

---
+++ assets/javascript/app.js
@@ -0,0 +1,41 @@
+// var arr = ['x', 'y','z'];
+
+// console.log(arr)
+
+function game() {
+
+  var quizContainer = $("#quiz");
+  var resultsContainer = $("#results");
+  var submitButton = $("#submit");
+  var myQuestions = [
+    {
+      question: "Who is the strongest?",
+      answers: {
+        a: "Superman",
+        b: "The Terminator",
+        c: "Waluigi, obviously"
+      },
+      correctAnswer: "c"
+    },
+    {
+      question: "What is the best site ever created?",
+      answers: {
+        a: "SitePoint",
+        b: "Simple Steps Code",
+        c: "Trick question; they're both the best"
+      },
+      correctAnswer: "c"
+    },
+    {
+      question: "Where is Waldo really?",
+      answers: {
+        a: "Antarctica",
+        b: "Exploring the Pacific Ocean",
+        c: "Sitting in a tree",
+        d: "Minding his own business, so stop asking"
+      },
+      correctAnswer: "d"
+    }
+  ];
+};
+game();
